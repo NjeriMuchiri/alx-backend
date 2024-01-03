55990bbd156a2ffaa645c583db0b9dec1e4bb986
NjeriMuchiri <muchirinjeri3210@gmail.com>
2024-01-03 23:01:53 +0300

    fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" FIFOCache class that inherits from BaseCache"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class"""
+
+    def __init__(self):
+        """ Initialize the FIFOCache"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.order.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    my_cache = FIFOCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
