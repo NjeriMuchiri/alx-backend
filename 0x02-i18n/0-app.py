b2578eab862d82010f00e783c01ebf7627a36003
NjeriMuchiri <muchirinjeri3210@gmail.com>
2024-01-10 00:03:00 +0300

    setting up our flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """rendering our html template method"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
